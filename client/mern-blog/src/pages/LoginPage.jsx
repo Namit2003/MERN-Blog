ccafde6238900d60cc86f4bc456500d54b873f4b
Namit <namit.k.patel@gmail.com>
2023-12-26 20:52:08 +0530

    day 2 login/register

--- client/mern-blog/src/pages/LoginPage.jsx
+++ client/mern-blog/src/pages/LoginPage.jsx
@@ -1,7 +1,48 @@
-const LoginPage = ()=>{
-    return(
-        <div>Login Page</div>
-    )
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const login = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:4000/login', {
+                method: 'POST',
+                body: JSON.stringify({ username, password }),
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include'
+            });
+
+            if (response.ok) {
+                setRedirect(true);
+            } else {
+                const responseData = await response.json();
+                setErrorMessage(responseData.error || 'Invalid credentials');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+            setErrorMessage('Invalid credentials');
+        }
+    };
+
+    if (redirect) {
+        return <Navigate to={'/'} />;
+    }
+
+    return (
+        <form className="login" onSubmit={login}>
+            <h1>Login</h1>
+            <input type="text" placeholder="username" value={username} onChange={event => setUsername(event.target.value)} />
+            <input type="password" placeholder="password" value={password} onChange={event => setPassword(event.target.value)} />
+            <button>Login</button>
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        </form>
+    );
 }
 
 export default LoginPage;
---
+++ client/mern-blog/src/pages/RegisterPage.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const RegisterPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState(null);
+
+    const register = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('http://localhost:4000/register', {
+                method: 'POST',
+                body: JSON.stringify({ username, password }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            if (!response.ok) {
+                const responseBody = await response.json();
+                if (responseBody.error) {
+                    setMessage(`Registration error: ${responseBody.error}`);
+                } else {
+                    setMessage('Unknown registration error');
+                }
+            } else {
+                setMessage('Registration successful');
+            }
+        } catch (error) {
+            setMessage(error.message);
+        }
+    };
+
+    return (
+        <form className="register" onSubmit={register}>
+            <h1>Register</h1>
+            <input type="text" placeholder="username" value={username} onChange={event => setUsername(event.target.value)} />
+            <input type="password" placeholder="password" value={password} onChange={event => setPassword(event.target.value)} />
+            <button>Register</button>
+            {message && <p>{message}</p>}
+        </form>
+    );
+};
+
+export default RegisterPage;
